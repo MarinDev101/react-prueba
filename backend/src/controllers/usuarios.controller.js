0ea942b4a62a5dd7d09b86f6a570dabaaa53d06e
Avi004 <juansgamer1@gmail.com>
2025-10-03 14:26:31 -0500

    fix: corregir errores usuarios controller

--- backend/src/controllers/usuarios.controller.js
+++ backend/src/controllers/usuarios.controller.js
@@ -1,4 +1,4 @@
-const db = require('./config/conexion.db');
+const db = require('../config/conexion_db');
 const bcrypt = require('bcrypt');
 
 class UsuariosController {
@@ -11,10 +11,10 @@ class UsuariosController {
                 FROM usuarios u
                 LEFT JOIN roles r ON u.id_rol = r.id_rol
                 LEFT JOIN rol_permiso rp ON r.id_rol = rp.id_rol
-                LEFT JOIN permisos p ON rp.id_permiso = p.id_permiso
+                LEFT JOIN permisos p ON rp.permiso_id = p.id_permiso
                 GROUP BY u.id_usuario, u.nombre, u.email, r.nombre`
             );
-            res.json({usuarios});
+            res.json({ usuarios });
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: 'Error al obtener usuarios' });
@@ -30,7 +30,7 @@ class UsuariosController {
                 FROM usuarios u
                 LEFT JOIN roles r ON u.id_rol = r.id_rol
                 LEFT JOIN rol_permiso rp ON r.id_rol = rp.id_rol
-                LEFT JOIN permisos p ON rp.id_permiso = p.id_permiso
+                LEFT JOIN permisos p ON rp.permiso_id = p.id_permiso
                 WHERE u.id_usuario = ?
                 GROUP BY u.id_usuario, u.nombre, u.email, r.nombre`,
                 [id]
@@ -38,7 +38,7 @@ class UsuariosController {
             if (usuario.length === 0) {
                 return res.status(404).json({ error: 'Usuario no encontrado' });
             }
-            res.json({usuario: usuario[0]});
+            res.json({ usuario: usuario[0] });
         } catch (error) {
             res.status(500).json({ error: 'Error al obtener usuario' });
         }
@@ -48,7 +48,6 @@ class UsuariosController {
     async agregarUsuario(req, res) {
         const { nombre, email, clave, id_rol } = req.body;
         try {
-            // Cifrar clave
             const hash = await bcrypt.hash(clave, 10);
 
             await db.query(
