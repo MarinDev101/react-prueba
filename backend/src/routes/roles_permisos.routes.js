8b3a7e1cf4fdcb48aaf2f42ac0f7754bcc5e26fb
Avi004 <juansgamer1@gmail.com>
2025-10-03 14:24:56 -0500

    fix: corregir errores roles permisos

--- backend/src/routes/roles_permisos.routes.js
+++ backend/src/routes/roles_permisos.routes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const RolPermisoController = require('../controllers/roles_permisos.controller');
 
 const router = express.Router();
-const RolPermisoController = new RolPermisoController();
+const rolPermisoController = new RolPermisoController();
 
-router.get('/', (req, res) => RolPermisoController.obtenerRolPermisos(req, res));
-router.get('/rol/:idrol', (req, res) => RolPermisoController.obtenerPermisosDeRol(req, res));
-router.get('/:id', (req, res) => RolPermisoController.obtenerRolPermisoPorId(req, res));
-router.post('/', (req, res) => RolPermisoController.agregarRolPermiso(req, res));
-router.put('/:id', (req, res) => RolPermisoController.actualizarRolPermiso(req, res));
-router.delete('/:id', (req, res) => RolPermisoController.eliminarRolPermiso(req, res));
+router.get('/', (req, res) => rolPermisoController.obtenerRolPermisos(req, res));
+router.get('/rol/:idrol', (req, res) => rolPermisoController.obtenerPermisosDeRol(req, res));
+router.get('/:id', (req, res) => rolPermisoController.obtenerRolPermisoPorId(req, res));
+router.post('/', (req, res) => rolPermisoController.agregarRolPermiso(req, res));
+router.put('/:id', (req, res) => rolPermisoController.actualizarRolPermiso(req, res));
+router.delete('/:id', (req, res) => rolPermisoController.eliminarRolPermiso(req, res));
 
 module.exports = router;
