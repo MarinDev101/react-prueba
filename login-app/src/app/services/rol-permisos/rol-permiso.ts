1e7ee1d733e2cda59935036b71dad937030cc41b
Avila004 <juansgamer1@gmail.com>
2025-10-04 17:54:33 -0500

    feat: añadir services rol-permisos

--- login-app/src/app/services/rol-permisos/rol-permiso.ts
+++ login-app/src/app/services/rol-permisos/rol-permiso.ts
@@ -1,8 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RolPermiso {
-  
+export class RolPermisoService {
+
+  private apiUrl = 'http://localhost:3000/api/rol-permiso';
+
+  constructor(private http: HttpClient) {}
+
+  obtenerRolPermisos(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  obtenerRolPermisosPorId(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  asigarPermiso(data: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, data);
+  }
+
+  actualizarRolPermiso(id: number, data: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${id}`, data);
+  }
+
+  eliminarRolPermisos(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  }
+
 }
