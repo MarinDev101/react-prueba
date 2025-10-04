3abca1820ff2291732696e6b015b98f04d32e2d1
Avila004 <juansgamer1@gmail.com>
2025-10-04 17:54:54 -0500

    feat: añadir services permisos

--- login-app/src/app/services/permisos/permisos.ts
+++ login-app/src/app/services/permisos/permisos.ts
@@ -1,8 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class Permisos {
-  
+export class PermisosService {
+
+  private apiUrl = 'http://localhost:300/api/permisos';
+
+  constructor(private http: HttpClient) {}
+
+  obtenerPermisos(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  obtenerPermisoPorId(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  agregarPermiso(permiso: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, permiso);
+  }
+
+  actualizarPermiso(id: number, permiso: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${id}`, permiso);
+  }
+
+  eliminarPermiso(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  }
+
 }
